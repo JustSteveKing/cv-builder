50d70effff918f671fcbcc82aea6d2a838dae7f2
Steve McDougall <juststevemcd@gmail.com>
2022-03-03 19:34:54 +0000

    Adding wire-elements/spotlight and PSR-12 formatting

--- database/migrations/2022_03_03_192911_create_profiles_table.php
+++ database/migrations/2022_03_03_192911_create_profiles_table.php
@@ -6,8 +6,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * @return void
      */
@@ -18,6 +17,8 @@ return new class extends Migration
             $table->uuid('uuid')->unique();
 
             $table->text('bio')->nullable();
+
+
 
             $table->timestamps();
         });
